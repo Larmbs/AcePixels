fc06e2af30fbec3f8a334309efb541568d3e4dda
Larmbs <138258207+Larmbs@users.noreply.github.com>
2023-06-30 18:55:29 -0400

    Create serve-json.php

---
+++ serve-json.php
@@ -0,0 +1,9 @@
+<?php
+$filename = 'yourfile.json';
+$maxAge = 3600; // Cache the file for 1 hour
+
+header('Content-Type: application/json');
+header('Cache-Control: max-age=' . $maxAge);
+
+readfile($filename);
+?>
